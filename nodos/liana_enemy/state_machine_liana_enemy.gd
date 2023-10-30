5e5d1dbc0f48f7f1f0b9526a6c802d330e9d4e9a
Francisco Cisneros <franbichir@gmail.com>
2023-10-30 14:50:24 -0300

    hecho el enemigo tipo liana

--- nodos/liana_enemy/state_machine_liana_enemy.gd
+++ nodos/liana_enemy/state_machine_liana_enemy.gd
@@ -1,15 +1,15 @@
 extends Node
 
-class_name state_machine_patrol_area_enemy
+class_name state_machine_liana_enemy
 
 @export var initial_state : State
-@export var Enemigo: CharacterBody2D
+@export var Enemigo: StaticBody2D
 
 var states :  Dictionary = {}
 var currentState : State
+var is_dead = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Enemigo.hitted.connect(on_hit_transition)
 	for child in get_children():
 		if child is State:
 			states[child.name.to_lower()] = child
@@ -20,7 +20,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	currentState.Update(delta)
-#	print(currentState)
+	if(Enemigo.health <= 0) and not is_dead:
+		Enemigo.set_rotation_degrees(180)
+		on_child_transition(currentState, "dead_state_liana_enemy")
+		is_dead = true
+	print(currentState)
 
 func _physics_process(delta):
 	currentState.UpdatePhysics(delta)
@@ -42,5 +46,3 @@ func on_child_transition(state, new_state_name):
 
 	currentState = new_state #haces que el nuevo estado sea el estado actual
 
-func on_hit_transition():
-	on_child_transition(currentState, "hitted_state_patrol_area_enemy")
