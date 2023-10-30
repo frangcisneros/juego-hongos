4725f304c288ecd1942269e478547e9ab13fd25a
Francisco Cisneros <franbichir@gmail.com>
2023-10-30 11:57:49 -0300

    hecho el knockback y el stagger del patrol area

--- nodos/patrol_area_enemy/hitted_state_patrol_area_enemy.gd
+++ nodos/patrol_area_enemy/hitted_state_patrol_area_enemy.gd
@@ -2,11 +2,14 @@ extends State
 
 class_name hitted_state_patrol_area_enemy
 
+@export var hitted_timer : Timer
+
 @onready var Enemigo = get_parent().Enemigo
 @onready var player = get_tree().get_nodes_in_group("player")[0]
 @onready var rest_timer = Enemigo.get_node("rest")
 
 var StateActive : bool = false
+var hitted = false
 
 func Enter():
 	Enemigo.velocity.y = -200
@@ -21,10 +24,13 @@ func Exit():
 	StateActive = false
 
 func UpdatePhysics(_delta:float):
-	pass
+	if Enemigo.is_on_floor() and not hitted:
+		Enemigo.velocity = Vector2.ZERO
+		hitted_timer.start()
+		hitted = true
 
 func Update(_delta : float):
-	if Enemigo.is_on_floor():
+	if Enemigo.is_on_floor() and hitted_timer.is_stopped():
 		Transition.emit(self,"idle_state_patrol_area_enemy")
 	
 	
