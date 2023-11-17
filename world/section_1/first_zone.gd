2a561df8f471837d3d54d182d617c924081fd048
JuanmaR0driguez <91332047+JuanmaR0driguez@users.noreply.github.com>
2023-11-17 14:23:00 -0300

    pruebas arte seccion 1

---
+++ world/section_1/first_zone.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var Camara : Camera2D = get_node("player/Camera2D")
+@onready var Rectangulo : CollisionShape2D =  get_node("limitesCamara/limitesCamaraShape")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	Camara.limit_left = -Rectangulo.shape.size.x/2+Rectangulo.position.x
+	Camara.limit_right = Rectangulo.shape.size.x/2+Rectangulo.position.x
+	Camara.limit_top = -Rectangulo.shape.size.y/2+Rectangulo.position.y
+	Camara.limit_bottom = Rectangulo.shape.size.y/2+Rectangulo.position.y
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
