1d3d176da8a21e10b51290f09a2b15a7be8b7311
Francisco Cisneros <106930707+frangcisneros@users.noreply.github.com>
2023-12-07 10:50:19 -0300

    el walk enemy ya no te hace daño una vez muerto

--- nodos/section 1/enemies/walk_enemy/dead_state_walk_enemy.gd
+++ nodos/section 1/enemies/walk_enemy/dead_state_walk_enemy.gd
@@ -3,11 +3,14 @@ extends State
 class_name dead_state_platform_enemy
 
 @onready var Enemigo = get_parent().Enemigo
+@onready var hitbox_cs = Enemigo.get_node("hitbox/CollisionShape2D")
 @export var corpse_timer : Timer
 var StateActive : bool = false
 # Called when the node enters the scene tree for the first time.
 
 func Enter():
+	hitbox_cs.set_deferred("disabled",true)
+	Enemigo.set_collision_mask_value(1,false)
 	Enemigo.set_rotation_degrees(180)
 	corpse_timer.start()
 	StateActive = true
